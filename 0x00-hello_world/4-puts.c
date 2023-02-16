bab288d7e6f6aff1ae3c01368cf9f2230bf89ce1
sageapus <lusandamposula@gmail.com>
2023-02-16 11:24:12 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- *  main - Entry Point
- *  
+ *  main - Entry
  *  Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 	puts("\"Programming is like builing a multilingual puzzle");
-
 	return(0);
 }
