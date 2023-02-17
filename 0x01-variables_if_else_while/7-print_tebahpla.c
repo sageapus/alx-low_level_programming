0196445cb0cfa792bdef578c21308e3b9f75b8c4
sageapus <lusandamposula@gmail.com>
2023-02-17 13:27:35 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+
+
+/**
+ * main - entry
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
+
