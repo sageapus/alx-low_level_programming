d4d36769df9d96e3a9ad03bf5a29bed73090e273
sageapus <lusandamposula@gmail.com>
2023-02-19 07:24:59 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - entry point
  *
- * Return: Always 1 
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
