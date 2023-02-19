4c6853e53e486782ec1ee35404912fb328bb7df3
sageapus <lusandamposula@gmail.com>
2023-02-19 07:06:41 -0800

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <unistd.h>
 
 
-/** main - a c programm that prints out a line to the standard error
+/**
+ * main - entry point
  *
  * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
