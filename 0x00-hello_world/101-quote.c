1e2cf557934d11cc6a5d3820d0274b0333d8f1eb
sageapus <lusandamposula@gmail.com>
2023-02-19 06:56:32 -0800

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/** main - entry point
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Koper, 2015-10-19\n",stdout);
+	return (1);
+}
