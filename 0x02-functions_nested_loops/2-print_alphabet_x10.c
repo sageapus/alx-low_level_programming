e1cac52b73f0a3c0d562d85778981bd5395d9ef7
sageapus <lusandamposula@gmail.com>
2023-02-22 13:11:37 -0800

    Lowercase x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,8 @@ void print_alphabet_x10(void)
 
 {
 	char c;
-	int i=0;
-	
+	int i = 0;
+
 	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
@@ -18,6 +18,5 @@ void print_alphabet_x10(void)
 		}
 			_putchar('\n');
 			i++;
-		
 	}
 }
