1543dffcca3e191f7b0d04a8c0fb1203d408e2bb
sageapus <lusandamposula@gmail.com>
2023-02-22 11:01:39 -0800

    Lowercase aplahbet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 
 /**
  * main - Entry point
- * 
  * Return: Always 0
  */
 
 void print_alphabet(void)
 {
 	char c;
-	for (c='a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
