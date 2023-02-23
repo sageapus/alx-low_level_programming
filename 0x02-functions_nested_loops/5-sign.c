fd8e19ab2a2b937751836be598d0ade48002ec6f
sageapus <lusandamposula@gmail.com>
2023-02-22 19:06:06 -0800

    is alpha

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the numberto be checked
+ * return: 1 for positivee num, -1 for negative num or zero for anything else
+ */
+
+int _isalpha(int c)
+{
+	if (n > 0)
+	{ 
+		_puchar(43);
+			return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
