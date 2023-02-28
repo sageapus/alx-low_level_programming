5284df5e7113ae77c7a195aa4ff8102b02845e59
sageapus <lusandamposula@gmail.com>
2023-02-28 08:58:10 -0800

    hate the code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,9 @@ void print_array(int *a, int n)
 
 	for (t = 0; t < n; t++)
 	{
-		printf("%d", a[t]);
-	}
+		printf("%d, " , a[t]);
 	if (t != (n - 1))
-	{
-		printf(", ");
+			printf(", ");
 	}
 	printf("\n");
 }
